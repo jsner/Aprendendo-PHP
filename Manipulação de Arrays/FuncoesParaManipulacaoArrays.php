0e150b3aecf3615c8846efd4612bb359b71c2b07
jsner <110409943+jsner@users.noreply.github.com>
2022-08-23 13:39:01 -0300

    Aprendendo PHP

--- Manipulação de Arrays/FuncoesParaManipulacaoArrays.php
+++ Manipulação de Arrays/FuncoesParaManipulacaoArrays.php
@@ -17,6 +17,60 @@ print_r($numeros);
 array_pop($numeros);
 print_r($numeros);
 
-# Retorna um array com os elementos na ordem inversa
+#  Retorna um array com os elementos na ordem inversa
 $reversed = array_reverse($numeros);
 print_r($reversed);
+
+#  Combina um ou mais arrays
+$a = array(1, 2, 3);
+$b = array('a', 'b', 'c');
+$c = array_merge($a, $b);
+
+print_r($c);
+
+#  Retorna todas as chaves ou uma parte das chaves de um array
+#  Segundo parametro => Se especificado, somente chaves contendo este valor são retornadas. 
+$array = array(0 => 100, "cor" => "vermelho");
+print_r(array_keys($array));
+
+$array = array("azul", "vermelho", "verde", "azul", "azul");
+print_r(array_keys($array, "azul"));
+
+#  Retorna todos os valores de um array
+print_r(array_values($array));
+
+#  Retorna a quantidade de elementos de um array
+echo count($array) . "\n";
+
+#  Verifique se um array possui um determinado valor
+$bebidas = ['refrigerante', 'suco', 'agua', 'vodka'];
+
+if (in_array('vodka', $bebidas)) {
+    echo " Vodka, temos! \n";
+}
+
+#  Ordena um array pelo seu valor
+$numeros_array = array(9,8,7,6,5,4,3,2,1);
+#sort($numeros_array);
+print_r($numeros_array);
+
+#  Ordena um array pelo seu valor, mantendo a associação de indices.
+asort($numeros_array);
+print_r($numeros_array);
+
+#  Ordena um array pelo seu indice
+
+$carro ['potencia'] = '1.0';
+$carro['ano'] = 'branco';
+$carro['modelo'] = 'celta';
+$carro['opcionais'] = 'ar quente';
+ksort($carro);
+print_r($carro);
+
+#  Converte um string em um array
+$string = '01/01/2001';
+print_r(explode('/', $string));
+
+#  Converte um array para string
+$array2 = [10, 5, 2016];
+print_r(implode('/', $array2));
